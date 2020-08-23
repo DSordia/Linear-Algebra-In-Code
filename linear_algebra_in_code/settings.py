134c24b55fa6da672ea5ec82eea5e8534ede3fae
DSordia <2kdavid2k@gmail.com>
2020-08-22 22:18:10 -0500

    Changed Settings

--- linear_algebra_in_code/settings.py
+++ linear_algebra_in_code/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '01ue8i0j%9r)o!hlp^padofrua$5f8rld1jt(0i59xa!4rf^qv'
+SECRET_KEY = '11ue8i0j%9r)o!hlp^padofrua$5f8rld1jt(0i59xa!4rf^qv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
